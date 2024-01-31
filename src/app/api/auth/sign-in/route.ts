c69ef43e3f90e2201589f27747af8dcecef975b4
HenrryHernandez <jenrruhndz@gmail.com>
2024-01-30 20:06:07 -0600

    feat (sign in): add api route for signing in

---
+++ src/app/api/auth/sign-in/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+import bcrypt from "bcrypt";
+
+import { generateToken } from "@/helpers";
+import db from "@/lib/db";
+
+export async function POST(request: NextRequest) {
+  const req = await request.json();
+
+  const { email, password } = req;
+
+  try {
+    const userFound = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userFound) {
+      return NextResponse.json({
+        status: 400,
+        msg: "The user doesn't exist",
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, userFound.password);
+
+    if (!validPassword) {
+      return NextResponse.json({
+        status: 400,
+        msg: "Invalid password",
+      });
+    }
+
+    const token = generateToken(email);
+
+    cookies().set("token", token, { httpOnly: true });
+
+    return NextResponse.json({ status: 200, msg: "Login succesfull" }, {});
+  } catch (error) {
+    return NextResponse.json({ status: 500 });
+  }
+}
