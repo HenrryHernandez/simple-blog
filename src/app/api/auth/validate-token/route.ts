f0cfa67fb1fb8dfea9484380f6a6d26d97d9871f
HenrryHernandez <jenrruhndz@gmail.com>
2024-01-31 11:40:12 -0600

    feat (token): add endpoint to validate token and get payload

---
+++ src/app/api/auth/validate-token/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { verifyTokenAndGetPayload } from "@/helpers";
+
+export async function GET(request: NextRequest) {
+  const req = request.cookies;
+  const token = req.get("token");
+
+  try {
+    if (!token) {
+      return NextResponse.json({ msg: "not token attached" }, { status: 401 });
+    }
+
+    const validToken = await verifyTokenAndGetPayload(token.value);
+
+    if (!validToken) {
+      return NextResponse.json({ msg: "not valid token" }, { status: 401 });
+    }
+
+    return NextResponse.json({
+      success: true,
+      data: validToken,
+      msg: "success",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, data: null, msg: "error" },
+      { status: 500 }
+    );
+  }
+}
